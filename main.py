4d0c86d4f7aef745d66a2960739c29ce75969b56
Dierdre Lockette <msdlockette@gmail.com>
2018-09-27 16:17:54 -0500

    added rotate string

---
+++ main.py
@@ -0,0 +1,50 @@
+from flask import Flask, request
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+form="""
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+      <form method = "post">
+      <label>Rotate by:</label>
+      <input type = "text" name = "rot" value = "0" />
+      <input type = "submit" />
+      <textarea name="text"></textarea>
+      
+      </form>
+    </body>
+</html>"""
+@app.route("/")
+def index():
+    return form
+
+@app.route("/label", methods = ['POST'])
+def encrypt():
+    text = str(text)
+    rot = str(rot)
+
+    return '<h1>rotate_string</h1>'
+
+
+
+app.run()
